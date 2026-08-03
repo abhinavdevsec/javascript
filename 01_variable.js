d550fe88401a1d933fc1a083d50b10b54aaa31f6
ABHINAV SINGH ADHIKARI <abhinavsinghadhikari4@gmail.com>
2026-08-03 18:55:50 +0000

    let cont and var

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = "abhi@google.com"
+var accountPassword = "45673"
+accountCity = "Jharkhand"
+let accountState;  // by default undefine
+
+accountEmail = "ac@google.com"
+accountPassword = "414141"
+accountCity = "Newyork"
+
+//accountId = 2;  // Not allowed in const
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) // Display output in tabular formate
+
+/*
+Prefer not to use var 
+
+*/
