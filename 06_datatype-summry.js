22cc9d6080744b7cc401560e15383ea172de3105
ABHINAV SINGH ADHIKARI <abhinavsinghadhikari4@gmail.com>
2026-09-08 11:18:01 +0000

    summary aslo expalined memory stack and heap in js

--- 06_datatype-summry.js
+++ 06_datatype-summry.js
@@ -41,3 +41,33 @@ const BigInt = 3456787578864675n
  }
 
  console.log(typeof outsideTemp); // All non primitive type return type is object
+
+
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+ // MEMORY ==> 1.Stack(Primitive) When we declare a stack memory the varible we dicrade we get excat copy of it, 
+ // 2.Heap(Non primitive) On the otherhand when we get decalre heap memory we get refrence of orignal value
+
+
+ let myName = "Goku"
+
+ let anothername = myName  // assining copy value 
+ anothername = "vegita"
+
+ console.log(myName);
+ console.log(anothername);
+
+
+ let userOne ={
+   email: "user@google.com",
+   upi: "user@ybl"
+ };
+
+
+ let userTwo = userOne  // assining refrence value 
+   userTwo.email = "Goku@google.com"
+
+   console.log(userOne.email);
+   console.log(userTwo.email);
+
+ 
